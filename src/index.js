a529bc316d5df054ca5f5b1811806ab3accbc74a
Borisenko Anton <aborisenko@xbsoftware.by>
2020-12-28 18:48:37 +0300

    [add] index js for app start

--- src/index.js
+++ src/index.js
@@ -1,33 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const AppHeader = () => {
-  return (
-    <h1>ToDo List</h1>
-  );
-}
-
-const SearchPanel = () => {
-  return <input placeholder="search" />
-}
-
-const TodoList = () => {
-  return (
-    <ul>
-      <li>Learn React</li>
-      <li>Build App</li>
-    </ul>
-  );
-}
-
-const App = () => {
-  return (
-    <div>
-      <AppHeader /> 
-      <SearchPanel />
-      <TodoList />
-    </div>
-  );
-}
+import App from './components/app'
 
 ReactDOM.render(<App />, document.getElementById('root'));
