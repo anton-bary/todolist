e49212d400c16077e52c6a4f4db7afa623bdc892
Borisenko Anton <aborisenko@xbsoftware.by>
2020-12-28 18:48:05 +0300

    [add] app components

---
+++ src/components/app/app.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import TodoList from '../todo-list';
+import ItemStatusFilter from '../item-status-filter';
+
+import './app.css';
+
+const App = () => {
+
+    const todoData = [
+        {id: "dc", label: "Drink Coffee", important: false},
+        {id: "dic", label: "Drink Irish Coffee", important: true},
+        {id: "dw", label: "Drink Whiskey", important: false},
+    ];
+
+    return (
+        <div className="todo-app">
+            <AppHeader toDo={1} done={3}/> 
+            <div className="top-panel d-flex">
+                <SearchPanel />
+                <ItemStatusFilter />
+            </div>
+            
+            <TodoList todos={ todoData } />
+        </div>
+    );
+}
+
+export default App;  
