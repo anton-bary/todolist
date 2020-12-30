ddaa327a962da2fd7bc3f798a47d33c05d614d3f
Borisenko Anton <aborisenko@xbsoftware.by>
2020-12-31 01:38:45 +0300

    [add] controlled element

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -17,6 +17,9 @@ export default class ItemAddForm extends Component {
     onSubmit = (e) => {
         e.preventDefault();
         this.props.onItemAdded(this.state.label);
+        this.setState({
+            label: ''
+        });
     };
 
     render() {
@@ -30,6 +33,7 @@ export default class ItemAddForm extends Component {
                     className="form-control"
                     onChange={this.onLabelChange}
                     placeholder="What needs to be done"
+                    value={this.state.label}
                 />
                 <button 
                     className="btn btn-outline-secondary"
